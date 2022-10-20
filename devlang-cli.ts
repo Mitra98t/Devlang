aac7f03d182286f6838f82baf2e1e320f06cf12f
Mitra98t <personalmailfm98@gmail.com>
2022-10-20 16:22:32 +0200

    finish cli

--- devlang-cli.ts
+++ devlang-cli.ts
@@ -1,29 +1,58 @@
-import { Parser } from "./Parser" 
+#!/usr/bin/env node
 
+import { Parser } from "./Parser" 
 import fs from "fs"
 
-function main(argv:any){
-  const [_node, _path, mode, exp, out, file] = argv
-  
-  let parser;
-  let ast;
+import chalk from "chalk";
+import clear from "clear"
+import figlet from "figlet"
+import {Command} from "commander"
+import { exit } from "process";
 
-  if(mode === '-e'){
-    parser = new Parser(exp)
-    ast = parser.parse()
-  }
+clear();
+console.log(
+  chalk.green(
+    figlet.textSync('DevLang', { horizontalLayout: 'full' })
+  )
+);
 
-  if(mode === '-f'){
-    const src = fs.readFileSync(exp, 'utf-8')
-    parser = new Parser(src)
-    ast = parser.parse()
-  }
+const program = new Command()
 
-  console.log(JSON.stringify(ast, null, 2))
-  
-  if(out == '-o'){
-    fs.writeFileSync(file, JSON.stringify(ast, null, 2))
-  }
+program
+  .version('0.0.1')
+  .description("DevLang-CLI")
+  .option('-e, --expression <exp>', 'Expression to Parse')
+  .option('-f, --file <file>', 'File to Parse')
+  .option('-o, --out <out file>', 'Output file to write ast to')
+  .option('-h, --help', 'Prints help command')
+  .parse(process.argv);
+
+//program.help()
+let opts = program.opts()
+
+let parser;
+let ast;
+
+if(opts.hasOwnProperty("help") || Object.keys(opts).length === 0 ){
+  program.help()
+  exit(0)
 }
 
-main(process.argv)
+if(!opts.hasOwnProperty("expression") && !opts.hasOwnProperty("file")){
+  console.log("Missing argument, use at least -e or -f")
+  exit(0);
+}
+
+if(opts.hasOwnProperty("expression")){
+  parser = new Parser(opts.expression)
+  ast = parser.parse()
+  console.log(JSON.stringify(ast, null, 2))
+}
+if(opts.hasOwnProperty("file")){
+  parser = new Parser(fs.readFileSync(opts.file, "utf-8"))
+  ast = parser.parse()
+  console.log(JSON.stringify(ast, null, 2))
+}
+if(opts.hasOwnProperty("out")){
+  fs.writeFileSync(opts.out, JSON.stringify(ast, null, 2))
+}
