4843d47bf23afddabd43fcdd9e4e45f4c63c12d5
Mitra98t <personalmailfm98@gmail.com>
2022-10-22 17:57:00 +0200

    finisced cli

--- devlang-cli.ts
+++ devlang-cli.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { Parser } from "./Parser" 
+import { Parser } from "./Parser"
 import fs from "fs"
 
 import chalk from "chalk";
 import clear from "clear"
 import figlet from "figlet"
-import {Command} from "commander"
+import { Command, Option } from "commander"
 import { exit } from "process";
 
 clear();
@@ -21,9 +21,10 @@ const program = new Command()
 program
   .version('0.0.1')
   .description("DevLang-CLI")
-  .option('-e, --expression <exp>', 'Expression to Parse')
-  .option('-f, --file <file>', 'File to Parse')
-  .option('-o, --out <out file>', 'Output file to write ast to')
+  .usage("[-e <exp> | -f <file>] ?-o <output file>")
+  .addOption(new Option('-e, --expression <exp>', 'Expression to Parse').conflicts(['file']))
+  .addOption(new Option('-f, --file <file>', 'File to Parse').conflicts(['expression']))
+  .option('-o, --out <output file>', 'Output file to write ast to (JSON format is used, so a json file is strongly raccomanded)')
   .option('-h, --help', 'Prints help command')
   .parse(process.argv);
 
@@ -33,26 +34,35 @@ let opts = program.opts()
 let parser;
 let ast;
 
-if(opts.hasOwnProperty("help") || Object.keys(opts).length === 0 ){
+if (opts.hasOwnProperty("help") || Object.keys(opts).length === 0) {
   program.help()
   exit(0)
 }
 
-if(!opts.hasOwnProperty("expression") && !opts.hasOwnProperty("file")){
+/*
+if (!opts.hasOwnProperty("expression") && !opts.hasOwnProperty("file")) {
   console.log("Missing argument, use at least -e or -f")
   exit(0);
 }
+*/
 
-if(opts.hasOwnProperty("expression")){
+console.log("")
+
+if (opts.hasOwnProperty("expression")) {
   parser = new Parser(opts.expression)
   ast = parser.parse()
-  console.log(JSON.stringify(ast, null, 2))
+  console.log(`Parsed expression: ${chalk.green(opts.expression)} into the following ${chalk.yellow("Abstract Syntax Tree")}\n`)
 }
-if(opts.hasOwnProperty("file")){
+if (opts.hasOwnProperty("file")) {
   parser = new Parser(fs.readFileSync(opts.file, "utf-8"))
   ast = parser.parse()
-  console.log(JSON.stringify(ast, null, 2))
+  console.log(`Parsed content of file: ${chalk.green(opts.file)} into the following ${chalk.yellow("Abstract Syntax Tree")}\n`)
 }
-if(opts.hasOwnProperty("out")){
+
+console.log(JSON.stringify(ast, null, 2))
+
+if (opts.hasOwnProperty("out")) {
   fs.writeFileSync(opts.out, JSON.stringify(ast, null, 2))
+  console.log(`Saved the AST into ${chalk.green(opts.out)}`)
 }
+
