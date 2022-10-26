229eabbd7b592bb93ea5065bd2274b5c0b6d2319
Mitra98t <personalmailfm98@gmail.com>
2022-10-26 12:53:15 +0200

    better cli

--- devlang-cli.ts
+++ devlang-cli.ts
@@ -20,12 +20,13 @@ console.log(
 const program = new Command()
 
 program
-  .version('0.0.1')
-  .description("DevLang-CLI")
-  .usage("[-e <exp> | -f <file>] ?-o <output file>")
-  .addOption(new Option('-e, --expression <exp>', 'Expression to Parse').conflicts(['file']))
-  .addOption(new Option('-f, --file <file>', 'File to Parse').conflicts(['expression']))
-  .option('-o, --out <output file>', 'Output file to write ast to (JSON format is used, so a json file is strongly raccomanded)')
+  .version('1.1.2')
+  .description("Proto-Language to try and create a parser and and interpreter in TypeScript")
+  .usage("[OPTIONS]")
+  .addOption(new Option('-e, --expression <exp>', 'Expression to Parse and Execute').conflicts(['file']))
+  .addOption(new Option('-f, --file <file.devl>', 'File to Parse and Execute').conflicts(['expression']))
+  .option('-p, --print', 'Prints to console the resulting AST of the devlang code.')
+  .option('-o, --out <output file for the AST>', 'Output file to write AST to (JSON format is used, so a .json file is strongly raccomanded)')
   .option('-h, --help', 'Prints help command')
   .parse(process.argv);
 
@@ -40,34 +41,32 @@ if (opts.hasOwnProperty("help") || Object.keys(opts).length === 0) {
   exit(0)
 }
 
-/*
-if (!opts.hasOwnProperty("expression") && !opts.hasOwnProperty("file")) {
-  console.log("Missing argument, use at least -e or -f")
-  exit(0);
-}
-*/
-
-console.log("")
-
 if (opts.hasOwnProperty("expression")) {
   parser = new Parser(opts.expression)
   ast = parser.parse()
-  console.log(`Parsed expression: ${chalk.green(opts.expression)} into the following ${chalk.yellow("Abstract Syntax Tree")}\n`)
 }
 if (opts.hasOwnProperty("file")) {
+  let fileArr = opts.file.split(".")
+  if (fileArr[fileArr.length - 1] != "devl") {
+    console.log(`Wrong file type. File must be ${chalk.yellow(".devl")}`)
+    exit(1)
+  }
   parser = new Parser(fs.readFileSync(opts.file, "utf-8"))
   ast = parser.parse()
-  console.log(`Parsed content of file: ${chalk.green(opts.file)} into the following ${chalk.yellow("Abstract Syntax Tree")}\n`)
 }
 
-console.log(JSON.stringify(ast, null, 2))
+if (opts.hasOwnProperty("print")) {
+  console.log(chalk.green(`\n---Parsed AST from code---\n`))
+  console.log(JSON.stringify(ast, null, 2))
+}
 
 if (opts.hasOwnProperty("out")) {
   fs.writeFileSync(opts.out, JSON.stringify(ast, null, 2))
-  console.log(`Saved the AST into ${chalk.green(opts.out)}`)
+  console.log(`\nSaved the AST into ${chalk.green(opts.out)}`)
 }
 
 console.log(chalk.green("\n---Execution---\n"))
 
+// Runner taking AST in input
 main(ast)
 
